63d3d671dd1398a0ea7b3760291822135f3432c4
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2020-11-19 14:09:29 +0700

    Update about.blade.php
    Update child template about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,14 @@
+@extends('template')
+
+@section('main')
 <div id="about">
     <h2>About</h2>
     <p>Alikasi <strong>laravelapp</strong> dibuat sebagai latihan untuk mempelajari Laravel.</p>
 </div>
+@stop
+
+@section('footer')
+	<div id="footer">
+		<p>&copy; 2016 laravelapp.dev</p>
+	</div>
+@stop
